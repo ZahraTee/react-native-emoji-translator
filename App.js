9b781b3adfd518ca1c9cde6aad6c1056c47dad45
zahra <zahratee@gmail.com>
2017-08-14 23:36:02 +0100

    Add moji-translate and basic function and design

--- App.js
+++ App.js
@@ -1,13 +1,32 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+
+var translate = require('moji-translate');
 
 export default class App extends React.Component {
+
+  state = {
+    translation: "...And emoji will appear here ✨"
+  }
+
+  translate(text){
+    translation = translate.translate(text, false);
+    this.setState({translation: translation});
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text style={styles.heading}>Millenial Talk</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={(text) => this.translate(text)}
+          multiline={true}
+          placeholder={"Get started by typing something..."}
+        ></TextInput>
+        <Text style={styles.input}>
+          {this.state.translation}
+        </Text>
       </View>
     );
   }
@@ -17,7 +36,23 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  heading: {
+    backgroundColor: '#ff0099',
+    color: 'white',
+    fontSize: 24,
+    padding: 10,
+    marginTop: 20,
+    width: '100%',
+    textAlign: 'center'
+  },
+  input: {
+    flex: 1,
+    margin: 10,
+    fontSize: 20,
+    borderColor: '#ff0099',
+    borderWidth: 2,
+    padding: 10
+  }
 });
