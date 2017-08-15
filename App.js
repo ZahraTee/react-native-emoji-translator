452b906438f26167bdfaad092851cd4d4bc19179
zahra <zahratee@gmail.com>
2017-08-15 14:38:26 +0100

    Add emoji-only and share options

--- App.js
+++ App.js
@@ -1,28 +1,52 @@
 import React from 'react';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button, Share } from 'react-native';
 
 var translate = require('moji-translate');
 
 export default class App extends React.Component {
 
   state = {
-    translation: "...And emoji will appear here ✨"
+    emojiOnly: false,
+    emojiOnlyText: "Emoji Only",
+    translation: "...And emoji will appear here ✨",
   }
 
-  translate(text){
-    translation = translate.translate(text, false);
+  shareTranslation = () => {
+    Share.share({
+      message: this.state.translation,
+      title: "Your Emoji translation"
+    })
+  }
+
+  translate = (text) => {
+    translation = translate.translate(text, this.state.emojiOnly);
     this.setState({translation: translation});
+  }
+
+  toggleEmojiOnly = () => {
+    this.setState({
+      emojiOnly: !this.state.emojiOnly,
+      emojiOnlyText: this.state.emojiOnly? "Text + Emoji" : "Emoji Only",
+      translation: this.translate(this.state.translation, this.state.emojiOnly)
+    });
+
   }
 
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.heading}>Millenial Talk</Text>
+        <View style={styles.options}>
+          <Button style={styles.optionButton}
+                  onPress={this.toggleEmojiOnly}
+                  title={this.state.emojiOnlyText} />
+          <Button style={styles.optionButton} onPress={this.shareTranslation} title={"Share"}/>
+        </View>
         <TextInput
           style={styles.input}
           onChangeText={(text) => this.translate(text)}
           multiline={true}
-          placeholder={"Get started by typing something..."}
+          placeholder="Get started by typing something..."
         ></TextInput>
         <Text style={styles.input}>
           {this.state.translation}
@@ -39,20 +63,29 @@ const styles = StyleSheet.create({
     justifyContent: 'center'
   },
   heading: {
-    backgroundColor: '#ff0099',
+    backgroundColor: '#CC333F',
     color: 'white',
     fontSize: 24,
     padding: 10,
-    marginTop: 20,
+    paddingTop: 24,
     width: '100%',
     textAlign: 'center'
   },
   input: {
     flex: 1,
     margin: 10,
+    marginTop: 0,
     fontSize: 20,
-    borderColor: '#ff0099',
+    borderColor: '#EDC951',
     borderWidth: 2,
     padding: 10
+  },
+  options: {
+    flexDirection: 'row',
+    backgroundColor: '#EB6841',
+    marginBottom: 10
+  },
+  optionButton: {
+    color: 'white'
   }
 });
